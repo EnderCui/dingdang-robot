b604cf1e9f776f4d6739d4df49223283220ac3b0
EnderCui <369@github>
2018-01-11 17:34:20 +0800

    TTS, ChatRobot API config information.

--- enderConfigFile.sh
+++ enderConfigFile.sh
@@ -1,14 +1,13 @@
 1. chat robot
-API Url: http://www.tuling123.com/openapi/api
 API Key: d161d58f8bff4723bf81e4dafd558665
 
-2. baidu TTS
+2. BD TTS
 App ID: 10675455
 API Key: DALlqtuvrK4ZUgU2idBBI3Qc
 Secret Key: c9e0b4439e1698be45b3d7eba359fc1b
 
-
-
-
+3. XZ Weather
+App ID: UEEB034924
+API Key: na88hdpdkegrfqzu
 
 
